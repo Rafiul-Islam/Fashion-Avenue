4af811484d74163d4d111e8b17e124d569a7b304
Rafiul-Islam <rafi.cse.ru@gmail.com>
2021-01-28 00:05:02 +0600

    add product to cart with redux done

--- src/Pages/Home/Products/ProductsPage.js
+++ src/Pages/Home/Products/ProductsPage.js
@@ -7,16 +7,16 @@ class ProductsPage extends Component {
 
     render() {
         return (
-            <div className="mx-3 mx-md-5 mt-5 mb-3 text-dark">
+            <div className="container mt-5 mb-3 text-dark">
                 <div className="row">
-                    <div className="col-md-8">
+                    <div className="col-md-12">
                         <Filter/>
                         <hr/>
                         <Products/>
                     </div>
-                    <div className="col-md-4">
-                        <Basket/>
-                    </div>
+                    {/*<div className="col-md-4">*/}
+                    {/*    <Basket/>*/}
+                    {/*</div>*/}
                 </div>
                 <hr className='my-5'/>
             </div>
--- src/Pages/ShoppingCart/Cart.js
+++ src/Pages/ShoppingCart/Cart.js
@@ -1,11 +1,67 @@
-import React from 'react';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import {addToCart, removeFromCart} from "../../actions/cartActions";
 
-const Cart = () => {
+class Cart extends Component {
+  render() {
+    const { cartItems } = this.props;
+
     return (
-        <div className='container mt-5'>
-            <h1>Cart</h1>
-        </div>
-    );
-};
+      <div className="alert alert-info container mt-5">
+        {cartItems.length === 0 ? (
+          "Basket is empty"
+        ) : (
+          <div>
+            You have {cartItems.length} items in the basket. <hr />
+          </div>
+        )}
+        {cartItems.length > 0 && (
+          <div>
+            <table className="table table-striped table-bordered">
+              <thead className="text-dark">
+              <tr>
+                <th scope="col">Product</th>
+                <th scope="col">Price</th>
+                <th scope="col">Count</th>
+                <th scope="col">Total</th>
+                <th scope="col"></th>
+              </tr>
+              </thead>
+              <tbody>
+              {cartItems.map((item, index) => (
+                  <tr key={index}>
+                    <td>{item.title}</td>
+                    <td><strong>$</strong>{item.price}</td>
+                    <td>{item.count}</td>
+                    <td><strong>$</strong>{item.price * item.count}</td>
+                    <button
+                        className='bg-danger rounded border-0 mt-2 text-white'
+                        onClick={(e) => this.props.removeFromCart(this.props.cartItems, item)}>
+                      x
+                    </button>
+                  </tr>
+              ))}
+              </tbody>
+            </table>
+            <hr/>
 
-export default Cart;
+            <h5 className='text-dark ml-2 font-weight-normal'>Total Price:&nbsp;<strong>$</strong>
+              {
+                cartItems.reduce((a, c) => a + c.price * c.count, 0)
+              }
+            </h5>
+            <button
+                className='btn btn-danger mt-3'
+                onClick={() => alert('Thanks for shopping with us!')}>
+              Checkout
+            </button>
+          </div>
+        )}
+      </div>
+    );
+  }
+}
+const mapStateToProps = (state) => ({
+  cartItems: state.cart.items,
+});
+export default connect(mapStateToProps, { addToCart, removeFromCart })(Cart);
