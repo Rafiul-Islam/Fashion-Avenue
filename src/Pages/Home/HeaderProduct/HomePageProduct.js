3c13cbaee7ba67d0cf5fc6fecd3e0561ad655265
Rafiul-Islam <rafi.cse.ru@gmail.com>
2021-01-28 14:15:54 +0600

    make the header product section mobile responsive

--- src/Pages/Home/HeaderProduct/HomePageProduct.js
+++ src/Pages/Home/HeaderProduct/HomePageProduct.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useHistory } from 'react-router-dom';
+import {useHistory} from 'react-router-dom';
 
 const HomePageProduct = () => {
 
@@ -10,32 +10,26 @@ const HomePageProduct = () => {
     }
 
     return (
-        <div className='mt-5 text-center'>
+        <div className='mt-5'>
             <div className='container'>
-                <div className="row mb-4">
-                    <div className="offset-md-1 col-md-3 my-2">
-                        <div className="card" style={{width: '18rem'}}>
-                            <img className="card-img-top rounded" src={`/products/pic1.png`}
-                                 alt="HeaderProductCard image cap"/>
-                        </div>
+                <div className="d-flex flex-wrap justify-content-center mb-4">
+                    <div className="card" style={{width: '18rem'}}>
+                        <img className="card-img-top rounded" src={`/products/pic1.png`}
+                             alt="HeaderProductCard image cap"/>
                     </div>
-
-                    <div className="col-md-3 my-2 ml-3">
-                        <div className="card" style={{width: '18rem'}}>
-                            <img className="card-img-top rounded" src={`/products/pic2.png`}
-                                 alt="HeaderProductCard image cap"/>
-                        </div>
+                    <div className="card" style={{width: '18rem'}}>
+                        <img className="card-img-top rounded" src={`/products/pic2.png`}
+                             alt="HeaderProductCard image cap"/>
                     </div>
-
-                    <div className="col-md-3 my-2 ml-3">
-                        <div className="card" style={{width: '18rem'}}>
-                            <img className="card-img-top rounded" src={`/products/pic3.png`}
-                                 alt="HeaderProductCard image cap"/>
-                        </div>
+                    <div className="card" style={{width: '18rem'}}>
+                        <img className="card-img-top rounded" src={`/products/pic3.png`}
+                             alt="HeaderProductCard image cap"/>
                     </div>
                 </div>
+                <div className='text-center'>
+                    <button className='btn btn-secondary mt-5' onClick={redirect}>Shop Now</button>
+                </div>
             </div>
-            <button className='btn btn-secondary mt-5' onClick={redirect}>Shop Now</button>
         </div>
     );
 };
--- src/Pages/Home/NewsLetter/NewsLetter.js
+++ src/Pages/Home/NewsLetter/NewsLetter.js
@@ -5,11 +5,11 @@ const NewsLetter = () => {
         <div className='mt-5 '>
             <div className='container'>
                 <div className='row'>
-                    <div className='col'>
+                    <div className='col-md-6'>
                         <h3 className='text-dark'>NewsLetter</h3>
                         <p className='text-dark'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est et excepturi inventore repellendus tempora tenetur?</p>
                     </div>
-                    <div className='col'>
+                    <div className='col-md-6'>
                         <form className="form-inline">
                             <div className="form-group mx-sm-3 mb-2">
                                 <label htmlFor="inputPassword2" className="sr-only">Password</label>
